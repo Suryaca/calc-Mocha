c7e4c8bb71074345ad0f18612c65a0fa6b0db351
Surya C. A <suryac.akasam@gmail.com>
2015-12-03 10:06:16 +0530

    Intial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var cal = require('./app/calc');
+
+var addResult = cal.add(5,2);
+var subResult = cal.sub(5,2);
+var mulResult = cal.mul(5,2);
+var divResult = cal.div(5,2);
+
+console.log(addResult);
+console.log(subResult);
+console.log(mulResult);
+console.log(divResult);
